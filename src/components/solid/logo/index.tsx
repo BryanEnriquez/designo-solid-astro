67fc863b25fc64de8187ab6430c4d0c7dd898a25
A27x <bryan.a.enriquez@gmail.com>
2022-10-15 21:58:15 -0400

    Update logo props

--- src/components/solid/logo/index.tsx
+++ src/components/solid/logo/index.tsx
@@ -2,12 +2,18 @@ import logoDark from '../../../images/shared/desktop/logo-dark.png';
 import logoLight from '../../../images/shared/desktop/logo-light.png';
 import style from './logo.module.scss';
 
-const Logo = (props: { color: 'dark' | 'light' }) => (
+type Props = {
+  color: 'dark' | 'light';
+  loading?: 'lazy' | 'eager';
+};
+
+const Logo = (props: Props) => (
   <a href="/" class={style.logo}>
     <img
       src={props.color === 'dark' ? logoDark : logoLight}
       alt="logo"
       class={style.logo__img}
+      loading={props.loading || 'lazy'}
     />
   </a>
 );
